3081d4a238ad8a063d207b5ecef00d54460222da
santiago <santithevenetvalles@gmail.com>
2023-08-04 12:19:47 +0200

    React Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold">Hola mundo</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/login" element={<h1>Login</h1>} />
+        <Route path="/register" element={<h1>Register</h1>} />
+        <Route path="/tasks" element={<h1>Tasks Page</h1>} />
+        <Route path="/add-task" element={<h1>New Task</h1>} />
+        <Route path="/task/:id" element={<h1>Update Task</h1>} />
+        <Route path="/profile" element={<h1>Profile</h1>} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
