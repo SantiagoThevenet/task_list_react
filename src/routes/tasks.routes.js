13d122d93f8fbdffe1dd205b141607a91f630e9e
santiago <santithevenetvalles@gmail.com>
2023-08-04 12:03:00 +0200

    Validaciones acabadas

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,14 +1,17 @@
 import { Router } from "express";
 import { authRequired } from "../middlewares/validateTokken.js";
 import { createTask, deleteTask, getTask, getTasks, updateTask } from "../controllers/task.controller.js";
+import { validateSchema } from '../middlewares/validator.middleware.js'
+import { createTaskSchema } from '../schemas/task.schema.js'
+
 
 const router = Router()
 
 router.get('/tasks', authRequired, getTasks)
 
-router.get('/task/:id', authRequired,getTask)
+router.get('/task/:id', authRequired, getTask)
 
-router.post('/task', authRequired, createTask)
+router.post('/task', authRequired, validateSchema(createTaskSchema), createTask)
 
 router.delete('/task/:id', authRequired, deleteTask)
 
