0b4e279e4db4b11bbc52e6e629c85db393a2a22c
santiago <santithevenetvalles@gmail.com>
2023-08-04 11:03:52 +0200

    CRUD complete

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs'
-import {createAccessToken} from '../libs/jwt.js'
+import { createAccessToken } from '../libs/jwt.js'
 
 export const register = async (req, res) => {
     try {
@@ -16,9 +16,9 @@ export const register = async (req, res) => {
 
         const userSaved = await newUser.save()
 
-        const token = await createAccessToken({id: userSaved._id})
+        const token = await createAccessToken({ id: userSaved._id })
 
-        res.cookie('token',token)
+        res.cookie('token', token)
 
         res.json({
             id: userSaved._id,
@@ -30,7 +30,7 @@ export const register = async (req, res) => {
         })
 
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 
 
@@ -40,18 +40,18 @@ export const login = async (req, res) => {
     try {
 
         const { email, password } = req.body
-        
-        const userFound = await User.findOne({email})
 
-        if (!userFound) return res.status(400).json({message: "User not found"})
-       
+        const userFound = await User.findOne({ email })
+
+        if (!userFound) return res.status(400).json({ message: "User not found" })
+
         const isMatch = await bcrypt.compare(password, userFound.password)
 
-        if (!isMatch) return res.status(400).json({message: "Incorrect password"})
-        
-        const token = await createAccessToken({id: userFound._id})
+        if (!isMatch) return res.status(400).json({ message: "Incorrect password" })
 
-        res.cookie('token',token)
+        const token = await createAccessToken({ id: userFound._id })
+
+        res.cookie('token', token)
 
         res.json({
             id: userFound._id,
@@ -63,7 +63,7 @@ export const login = async (req, res) => {
         })
 
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 
 
@@ -75,4 +75,19 @@ export const logout = (req, res) => {
     })
 
     return res.sendStatus(200)
+}
+
+export const profile = async (req, res) => {
+    const userFound = await User.findById(req.user.id)
+    if (!userFound) return res.status(400).json({message: "User not found"})
+    
+    return res.json({
+        id: userFound._id,
+        username: userFound.username,
+        email: userFound.email,
+        createdAt: userFound.createdAt,
+        updatedAt: userFound.updatedAt,
+    })
+
+    
 }
---
+++ src/middlewares/validateTokken.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken"
+import { TOKEN_SECRET } from "../config.js"
+export const authRequired = (req, res, next) => {
+    const { token } = req.cookies
+    
+    if (!token)
+        return res.status(404).json({ message: "No token, autorization denied" })
+    
+        jwt.verify(token, TOKEN_SECRET, (err, user) => {
+        if (err) return res.status(403).json({ message: "Invalid token" })
+        
+        req.user = user
+        
+        next()
+    })
+}
